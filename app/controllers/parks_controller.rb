966abb61e255829a45ba3d38e0aceca0c92d743f
Young Ju Park <parkyngj@gmail.com>
2016-10-29 18:49:10 -0700

    Implement functionality to create parks

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -7,4 +7,25 @@ class ParksController < ApplicationController
   def show
     @park = Park.find(params[:id])
   end
+
+  def new
+    @city = City.find(params[:city_id])
+  end
+
+  def create
+    @city = City.find(params[:city_id])
+    @park = @city.parks.new(park_params)
+    @park.creator = current_user
+    if @park.save
+      redirect_to city_park_path(@city, @park)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def park_params
+    params.require(:park).permit(:name, :description, :city, :creator)
+  end
 end
